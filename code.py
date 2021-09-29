efbb718683dab131eeed8168ce9722f7f6bb259e
alg <alg.info@gmail.com>
2021-09-29 18:36:16 +0900

    Cleaner key names

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-#PiPi-GHERKIN - Raspberry Pi PICO
+#PiPi-keyboard - Raspberry Pi PICO
 import board
 from kmk.keys import KC
 from kmk.kmk_keyboard import KMKKeyboard
@@ -7,31 +7,36 @@ from kmk.hid import HIDModes
 from kmk.modules.layers import Layers
 from kmk.modules.modtap import ModTap
 
-gherkin = KMKKeyboard()
-gherkin.modules.append(Layers())
-gherkin.modules.append(ModTap())
+keyboard = KMKKeyboard()
 
-gherkin.col_pins = (board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7)
-gherkin.row_pins = (board.GP8, board.GP9, board.GP10, board.GP11, board.GP12)
+modtap = ModTap()
+layers_ext = Layers()
+keyboard.modules = [layers_ext, modtap]
 
-gherkin.diode_orientation = DiodeOrientation.COLUMNS
+keyboard.col_pins = (board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7)
+keyboard.row_pins = (board.GP8, board.GP9, board.GP10, board.GP11, board.GP12)
 
-gherkin.debug_enabled = False
+keyboard.diode_orientation = DiodeOrientation.COLUMNS
 
-gherkin.keymap = [
+keyboard.debug_enabled = False
+
+# Cleaner key names
+CTL_Z = KC.MT(KC.Z, KC.LCTRL)
+ALT_X = KC.MT(KC.X, KC.LALT)
+LT3_C = KC.LT(3, KC.C)
+LT4_V = KC.LT(4, KC.V)
+LT2_B = KC.LT(2, KC.B)
+LT1_N = KC.LT(1, KC.N)
+LT5_M = KC.LT(5, KC.M)
+ALT_SPC = KC.MT(KC.SPC, KC.RALT)
+CTL_BS = KC.MT(KC.BSPC, KC.RCTRL)
+SFT_ESC = KC.MT(KC.ESC, KC.RSFT)
+
+keyboard.keymap = [
     [#L0
         KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,
         KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.ENT,
-        KC.MT(KC.Z, KC.LCTRL),
-                 KC.MT(KC.X, KC.LALT),
-                          KC.LT(3, KC.C),
-                                   KC.LT(4, KC.V),
-                                            KC.LT(2, KC.B),
-                                                     KC.LT(1, KC.N),
-                                                              KC.LT(5, KC.M),
-                                                                       KC.MT(KC.SPC, KC.RALT),
-                                                                                KC.MT(KC.BSPC, KC.RCTRL),
-                                                                                         KC.MT(KC.ESC, KC.RSFT),
+        CTL_Z,   ALT_X,   LT3_C,   LT4_V,   LT2_B,   LT1_N,   LT5_M,   ALT_SPC, CTL_BS,  SFT_ESC,
     ],
     [#L1
         KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,
@@ -61,4 +66,4 @@ gherkin.keymap = [
 ]
 
 if __name__ == '__main__':
-    gherkin.go(hid_type=HIDModes.USB) #Wired USB enable
+    keyboard.go(hid_type=HIDModes.USB) #Wired USB enable
